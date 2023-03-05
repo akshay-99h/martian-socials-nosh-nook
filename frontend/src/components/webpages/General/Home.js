a28b967e81909169ad29f1d04083cc6939f0745f
Tushar <tush.sharma2094@gmail.com>
2023-03-05 10:22:57 +0530

    Added New Things

--- frontend/src/components/webpages/General/Home.js
+++ frontend/src/components/webpages/General/Home.js
@@ -7,16 +7,14 @@ import Spline from '@splinetool/react-spline';
 
 const Home = () => {
     const matches = useMediaQuery('(min-width:480px)');
-
+    const myStyle={ backgroundImage : "url(/martian-socials-nosh-nook\images\BG_mars)"};
     return (
         <div>
-            {!user_is_authenticated() ?
-                matches ?
+            
                     <div className="welcome-page">
                         <Spline scene="https://prod.spline.design/XBgGDAHTrMH1APWP/scene.splinecode" />
                         
- const myStyle={
-        backgroundImage: "url(/martian-socials-nosh-nook\images\BG_mars)",
+ 
 
                         <Typography className="welcome-heading" variant="h2" component="h1">
                             Welcome to Martian Social's Nosh Nook!
@@ -27,22 +25,7 @@ const Home = () => {
                         <BuyerDashboard />
                     </div>
                     
-                    :
-                    
-                    <div className="welcome-page">
-                        <BuyerDashboard />
-                        <Typography className="welcome-heading" variant="h4" component="h1">
-                            Welcome to the Food Ordering Portal
-                        </Typography>
-                        <Typography variant="h6" component="h1">
-                            Please login or register to continue
-                        </Typography>
-                    </div>
-                :
-                <div>
-                        <BuyerDashboard />
-                </div>
-            }
+            
             <div>
     <div class="carousel">
         <div class="carousel-item">
